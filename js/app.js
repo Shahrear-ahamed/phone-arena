f1aadb786ddfa26f1565d368af892a1055592919
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-02-28 21:41:19 +0600

    load data successfully by search (only show in console)

--- js/app.js
+++ js/app.js
@@ -7,4 +7,21 @@
 // URL Format: https://openapi.programming-hero.com/api/phone/${id}
 
 // Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
+const loadPhoneDataSearches = (searchPhone) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showData(data.data));
+};
 
+const loadSearch = () => {
+  const searchValue = document.getElementById("search-phone");
+  loadPhoneDataSearches(searchValue.value);
+  // searchValue.value ="";
+};
+
+const showData = (phones) => {
+  phones.forEach((phone) => {
+    console.log(phone);
+  });
+};
