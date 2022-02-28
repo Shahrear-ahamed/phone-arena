c5e89106b60751a730a7f125eea204d20a1e633c
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-03-01 00:01:03 +0600

    load mobile details on console

--- js/app.js
+++ js/app.js
@@ -1,27 +1,26 @@
-// Phone Search
-// URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
-
-// Example: https://openapi.programming-hero.com/api/phones?search=iphone
-
 // Phone detail url:
-// URL Format: https://openapi.programming-hero.com/api/phone/${id}
+// URL Format: 
 
 // Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
 const loadPhoneDataSearches = (searchPhone) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => showData(data.data));
+    .then((data) => showData(data.data.slice(0, 20)));
 };
+const loadOneMobile=(modelNumber) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${modelNumber}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>mobileDetails(data));
+}
 
 const loadSearch = () => {
   document.getElementById("phone-result").textContent = "";
   const searchValue = document.getElementById("search-phone");
-  if (searchValue.value === "") {
-    alert("Please search brand name");
-  } else {
-    loadPhoneDataSearches(searchValue.value);
-  }
+  const searchResult= document.getElementById("search-result");
+  searchResult.innerText=searchValue.value;
+  loadPhoneDataSearches(searchValue.value);
   searchValue.value = "";
 };
 
@@ -35,10 +34,19 @@ const showData = (phones) => {
         <h2 class="text-2xl mt-4 font-semibold">${phone.phone_name}</h2>
         <div class="flex justify-between mx-7 mt-5 mb-3">
             <h4 class="text-lg font-semibold">Brand: ${phone.brand}</h4>
-            <button class="bg-blue-600 text-white py-1 px-5 rounded" onClick="showDetails()">Details</button>
+            <button class="bg-blue-600 text-white py-1 px-5 rounded" onClick="showDetails('${phone.slug}')">Details</button>
         </div>
     </div>
     `;
     phoneResult.appendChild(div);
   });
+  
+  document.getElementById("result-title").classList.remove("hidden");
 };
+
+const showDetails =(id)=> {
+    loadOneMobile(id);
+}
+const mobileDetails =(mobile)=> {
+    console.log(mobile);
+}
